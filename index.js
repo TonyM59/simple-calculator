7fe22605907008f4acd402c155812d9a38c6f541
TonyM59 <tonymahlangu59@gmail.com>
2021-06-15 16:26:18 +0200

    added the additon and division functions

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function multiply(a, b) {
+function multiply(a, b,) {
     let multiply = a * b;
     console.log(multiply);
 }
-multiply(3, 3)
+multiply(3, 3);
+
+function addition(a, b, c){
+    console.log(a + b + c);
+}
+addition (10, 5, 3);
+
+function division(a,b){
+    console.log(a / b);
+}
+division (32, 2);
